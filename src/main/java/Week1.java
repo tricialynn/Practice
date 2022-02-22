7652d69167da5fb53b76b53743679ecfc32806c1
tricialynn <plg1003@wildcats.unh.edu>
2022-02-22 18:04:17 -0500

    adding coding challenge for week one of PO

---
+++ src/main/java/Week1.java
@@ -0,0 +1,30 @@
+
+public class Week1 {
+	public static void main(String[] args) {
+		String ans = "192384576";
+		int answer = 192;
+		String tempAns = "";
+		
+		for(int i = 1; i<100000; i++) {
+			int j = 1;
+			do {
+				
+				tempAns = tempAns + Integer.toString(i*j);
+				j++;
+			}while(tempAns.length()<9);
+			
+			if(tempAns.length() == 9 && (tempAns.contains("1")&&(tempAns.contains("2")&&(tempAns.contains("3")&&(tempAns.contains("4")&&(tempAns.contains("5")&&(tempAns.contains("6")&&(tempAns.contains("7")&&(tempAns.contains("8")&&tempAns.contains("9")))))))))){
+
+				if(Integer.parseInt(tempAns)>Integer.parseInt(ans)) {
+					ans = tempAns;
+					answer = i;
+				}
+			}
+			tempAns = "";
+				
+			
+		}
+		System.out.println("Answer is "+ answer);
+		System.out.println("Pandigital 9-digit number is "+ ans);
+	}
+}
